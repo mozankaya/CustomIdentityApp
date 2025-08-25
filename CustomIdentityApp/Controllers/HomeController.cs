680a2dff12b5f1fedcf4347e1f8832d03880ccf8
mozankaya <mehmetozankaya@gmail.com>
2025-08-26 00:31:15 +0300

    add AccountController and Views

--- CustomIdentityApp/Controllers/HomeController.cs
+++ CustomIdentityApp/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 using System.Diagnostics;
 using CustomIdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CustomIdentityApp.Controllers
 {
+
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
